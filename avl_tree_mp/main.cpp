5487019b14162ed5bd4fcbc859a63ac30e01e8d8
Mateusz Przeliorz <mateuszprzeliorz@outlook.com>
2018-03-10 20:47:07 +0100

    Added AVL(insertion)

--- avl_tree_mp/main.cpp
+++ avl_tree_mp/main.cpp
@@ -3,14 +3,22 @@ using namespace std;
 
 
 
-template<typename T>
+template<class T>
 struct NodeAVL
 {
 	NodeAVL<T>* left;
 	NodeAVL<T>* right;
 	T data;
-
+	int height;
+	NodeAVL();
 };
+
+
+template<class T>
+NodeAVL<T>::NodeAVL()
+{
+	height = 1;   //always start node with height = 1
+}
 
 
 template<class T>
@@ -19,11 +27,15 @@ class AVLTree
 public:
 	NodeAVL<T> *insert(NodeAVL<T> *root, T value);
 	NodeAVL<T> *remove(NodeAVL<T> *root, T value);
+	NodeAVL<T> *RR(NodeAVL<T> *root);
+	NodeAVL<T> *LR(NodeAVL<T> *root);
+	int getHeight(NodeAVL<T> *root);
+	int max(int first, int second);
+	int checkBalace(NodeAVL<T> *root);
 	void display(NodeAVL<T> *root, int start);
 	void inorder(NodeAVL<T> *tree);
 
 };
-
 
 
 template<class T>
@@ -44,9 +56,31 @@ NodeAVL<T>* AVLTree<T>::insert(NodeAVL<T> *root, T value)
 	{
 		root->right = insert(root->right, value);
 	}
+
+	root->height = 1 + max(getHeight(root->left), getHeight(root->right));
+
+	int balance = checkBalace(root);
+
+	if (balance > 1 && value < root->left->data)
+		return RR(root);
+
+	if (balance < -1 && value > root->right->data)
+		return LR(root);
+
+	if (balance > 1 && value > root->left->data)
+	{
+		root->left = LR(root->left);
+		return RR(root);
+	}
+
+	if (balance < -1 && value < root->right->data)
+	{
+		root->right = RR(root->right);
+		return LR(root);
+	}
+
 	return root;
 }
-
 
 
 template<class T>
@@ -56,54 +90,116 @@ NodeAVL<T>* AVLTree<T>::remove(NodeAVL<T> *root, T value)
 	{
 		return root;
 	}
-	else if (value < root->data)
+	else if (value < root->data)  //Try on the left side
 	{
 		root->left = remove(root->left, value);
 	}
-	else if (value > root->data)
+	else if (value > root->data)  //Try on the right side
 	{
 		root->right = remove(root->right, value);
 	}
-	else
+	else  //If the value passed is equal to the element that I want to remove  
 	{
 		//Zero or one child
-		if (root->left == NULL)
+		if (root->left == NULL)  
 		{
 			NodeAVL<T>* temporary = root->right;
 			delete root;
-			return temporary;
+			return temporary; //Replacing a deleted item with his child
 		}
 		else if (root->right == NULL)
 		{
 			NodeAVL<T> *temporary = root->left;
 			delete root;
-			return temporary;
+			return temporary; //Replacing a deleted item with his child
 		}
 		//Two or more childs
+		else
+		{
+			NodeAVL<T>* temp;
+			temp = root->right;
 
-		NodeAVL<T>* temp;
-		temp = root->right;
+			while (temp->left != NULL) // Finding the lowest element of the right child
+			{
+				temp = temp->left;
+			}
 
-		while (temp->left != NULL)
-		{								//Finding the lowest element of the right child
-			temp = temp->left;
+			root->data = temp->data;
+			root->right = remove(root->right, temp->data);  //Finding the old element, that we dont need anymore
 		}
-
-		root->data = temp->data;
-		root->right = remove(root->right, temp->data);
 	}
 	return root;
 }
 
+template<class T>
+NodeAVL<T>* AVLTree<T>::RR(NodeAVL<T> *root)
+{
+	NodeAVL<T> *firstChild = root->left;
+	NodeAVL<T> *nextChild = firstChild->right;
+
+	firstChild->right = root;
+	root->left = nextChild;
+
+	//change heights
+	firstChild->height = max(getHeight(firstChild->left), getHeight(firstChild->right)) + 1;
+	root->height = max(getHeight(root->left), getHeight(root->right)) + 1;
+
+	return firstChild;
+}
+
+
+
 
 template<class T>
-void AVLTree<T>::inorder(NodeAVL<T> *tree)
+NodeAVL<T>* AVLTree<T>::LR(NodeAVL<T> *root)
 {
-	if (tree == NULL)
+	NodeAVL<T> *firstChild = root->right;
+	NodeAVL<T> *nextChild = firstChild->left;
+
+	firstChild->left = root;
+	root->right = nextChild;
+
+	//change heights
+	firstChild->height = max(getHeight(firstChild->left), getHeight(firstChild->right)) + 1;
+	root->height = max(getHeight(root->left), getHeight(root->right)) + 1;
+
+	return firstChild;
+}
+
+template<class T>
+int AVLTree<T>::getHeight(NodeAVL<T>* root)
+{
+	if (!root)
+		return 0;
+	return root->height;
+}
+
+
+template<class T>
+int AVLTree<T>::max(int first, int second)
+{
+	if (first >= second)
+		return first;
+	return second;
+}
+
+template<class T>
+int AVLTree<T>::checkBalace(NodeAVL<T>* root)
+{
+	if (!root)
+		return 0;
+	return (getHeight(root->left) - getHeight(root->right));
+}
+
+
+template<class T>
+void AVLTree<T>::inorder(NodeAVL<T> *root)
+{
+	if (root == NULL)
 		return;
-	inorder(tree->left);
-	cout << tree->data << "  ";
-	inorder(tree->right);
+	inorder(root->left);
+	cout << root->data << "  ";
+	inorder(root->right);
 }
 
 
@@ -128,16 +224,24 @@ int main()
 {
 	NodeAVL<int> *root = NULL;
 	AVLTree<int> start;
+
 	root = start.insert(root, 50);
-	root = start.insert(root, 30);
-	root = start.insert(root, 12);
-	root = start.insert(root, 20);
-	root = start.insert(root, 10);
+	root = start.insert(root, 49);
+	root = start.insert(root, 48);
+	root = start.insert(root, 47);
+	root = start.insert(root, 45);
+	root = start.insert(root, 44);
+	root = start.insert(root, 43);
+	root = start.insert(root, 42);
+	root = start.insert(root, 41);
+	root = start.insert(root, 40);
+	root = start.remove(root, 50);
 
 
-	root = start.remove(root, 30);
+
 
 	start.display(root, 0);
+
 
 
 	getchar();
